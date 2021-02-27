1208bfbe03a2cf31869e8abf64245977972db677
wangxyper <79621885+wangxyper@users.noreply.github.com>
2021-02-27 17:13:08 +0800

    Update MainServer.py

--- MainServer.py
+++ MainServer.py
@@ -4,35 +4,35 @@ global s #初始化部分全局变量
 s = 0
 global data
 data = 0
-from socket import *
+from socket import * #导入依赖
 from time import  ctime
 import  threading
 import time
-host = ServerConfig.Server_IP
+host = ServerConfig.Server_IP #定义服务端IP&&端口
 port = ServerConfig.Server_Port
 ADDR = (host,port)
-serverlooptick = True
-tcpSerSock=socket(AF_INET,SOCK_STREAM)
+serverlooptick = True #无用变量
+tcpSerSock=socket(AF_INET,SOCK_STREAM) #固定服务端地址&&监听部分
 tcpSerSock.bind(ADDR)
-tcpSerSock.listen(ServerConfig.Server_max_listen)
+tcpSerSock.listen(ServerConfig.Server_max_listen) 
 socks=[]                             #放每个客户端的socket
 
-def handle(): #SOCKET部分
+def handle(): #SOCKET（服务端SOCKET通信）部分
     while True:
-        global s
-        for s in socks:
+        global s #定义变量S为全局变量
+        for s in socks: #for循环
             try:
-                data = s.recv(ServerConfig.Server_BUFSIZ)     #到这里程序继续向下执行
-            except Exception, e:
-                continue
-            if not data:
+                data = s.recv(ServerConfig.Server_BUFSIZ)     #到这里程序继续向下执行 #定义data为服务端接收到的数据
+            except Exception, e: 
+                continue #向下继续
+            if not data: #超时断开
                 socks.remove(s)
-                continue
-            s.send('[%s],%s' % (ctime(), data))
+                continue #向下继续
+            s.send('[%s],%s' % (ctime(), data)) #发送时间戳
 
 def Mainserver(): #服务端部分
-    print "Server Loaded!"
-    while serverlooptick:
+    print "Server Loaded!" #打印Server Loaded
+    while serverlooptick: #服务端循环TICK
      if not ServerConfig.Server_Password and data:
         s.send('Server-Version:1.0-test')
         if data == 'ClientOK':
@@ -44,14 +44,14 @@ def Mainserver(): #服务端部分
                  s.send('Data-Started')
 
 
-
+#为服务端和SOCKET部分创建线程
 t = threading.Thread(target=handle)
 t1 = threading.Thread(target=Mainserver)       #子线程
-if __name__ == '__main__':   
+if __name__ == '__main__':  #初始化  
     t.start()
     t1.start()
     print 'waiting for connecting...' #SOCKET部分
-    while True:
+    while True: #非阻塞式accept
         clientSock,addr = tcpSerSock.accept()
         print  'connected from:', addr
         clientSock.setblocking(0)
